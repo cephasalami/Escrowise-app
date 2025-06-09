9f6f03f23a597e071e3636edc8c6812c54b5f27b
Cephasalami <Oluwatayo.salami@gmail.com>
2025-06-09 14:37:37 +0100

    fix: add custom type declarations for @heroicons/react/solid and /outline, update tsconfig include

---
+++ types/heroicons.d.ts
@@ -0,0 +1,8 @@
+declare module '@heroicons/react/solid' {
+  const content: any;
+  export default content;
+}
+declare module '@heroicons/react/outline' {
+  const content: any;
+  export default content;
+}
