f4ebc143113f584fb7906ec39b733c28e841c3ac
Cephasalami <cephasalami@gmail.com>
2025-06-17 09:39:35 +0100

    fix: export TextareaProps from Textarea component for type-safe usage and compatibility

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -2,9 +2,11 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+export type TextareaProps = React.ComponentProps<'textarea'>;
+
 const Textarea = React.forwardRef<
   HTMLTextAreaElement,
-  React.ComponentProps<"textarea">
+  TextareaProps
 >(({ className, ...props }, ref) => {
   return (
     <textarea
